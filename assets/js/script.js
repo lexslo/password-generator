df9825786b7bf50bb8658ccb90ebfc9496881828
Sparkle Party <lexi.slovik@gmail.com>
2021-11-13 17:12:53 -0800

    pseudocode script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
 
+// when generate btn is clicked -> prompt for criteria
+// after criteria, prompt for PW length (between 8 and 128 characters)
+// prompt for character types -> include lowercase? uppercase? numbers? special characters?
+
+// each prompt should be validated
+// at least one character type should be selected
+
+// generate password after all prompts are answered
+// password is displayed as an alert OR written to the page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
